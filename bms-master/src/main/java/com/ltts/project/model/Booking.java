782b932042442c2ee267299bbe2b180fb40ec221
VinayakAlagwadi <vinayak.alagwadi@ltts.com>
2021-03-20 14:30:52 +0530

    updATE

---
+++ bms-master/src/main/java/com/ltts/project/model/Booking.java
@@ -0,0 +1,105 @@
+package com.ltts.project.model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "booking")
+public class Booking {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int bookedSeats;
+	private String date;
+	private String time;
+	
+	private int total;
+	@ManyToOne
+	private Movie movie;
+	@ManyToOne
+	private Screen screen;
+	@ManyToOne
+	@JoinColumn(name="email")
+	private Member user;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getBookedSeats() {
+		return bookedSeats;
+	}
+	public void setBookedSeats(int bookedSeats) {
+		this.bookedSeats = bookedSeats;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public Movie getMovie() {
+		return movie;
+	}
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+	public Screen getScreen() {
+		return screen;
+	}
+	public void setScreen(Screen screen) {
+		this.screen = screen;
+	}
+	public Member getUser() {
+		return user;
+	}
+	public void setUser(Member user) {
+		this.user = user;
+	}
+	public Booking(int id, int bookedSeats, String date, String time, int total, Movie movie, Screen screen,
+			Member user) {
+		super();
+		this.id = id;
+		this.bookedSeats = bookedSeats;
+		this.date = date;
+		this.time = time;
+		this.total = total;
+		this.movie = movie;
+		this.screen = screen;
+		this.user = user;
+	}
+	public Booking() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Booking [id=" + id + ", bookedSeats=" + bookedSeats + ", date=" + date + ", time=" + time + ", total="
+				+ total + ", movie=" + movie + ", screen=" + screen + ", user=" + user + "]";
+	}
+	
+	
+	
+	
+}
